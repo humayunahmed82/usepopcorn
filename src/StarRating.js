c67d988fc575430997d7e375c2a1b45814a779f2
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2023-08-31 14:26:04 +0600

    Rating Function Done

--- src/StarRating.js
+++ src/StarRating.js
@@ -7,6 +7,7 @@ const startRow = {
 
 const StarRating = ({ maxRating = 5 }) => {
     const [starRating, setStartRating] = useState(0)
+    const [tempRating, setTempRating] = useState(0)
 
     const handelRated = (rating) => {
         setStartRating(rating)
@@ -16,10 +17,20 @@ const StarRating = ({ maxRating = 5 }) => {
         <div>
             <div style={startRow}>
                 {Array.from({ length: maxRating }, (_, i) => (
-                    <Start key={i} onRate={() => handelRated(i + 1)} />
+                    <Start
+                        key={i}
+                        full={
+                            tempRating
+                                ? tempRating >= i + 1
+                                : starRating >= i + 1
+                        }
+                        onRate={() => handelRated(i + 1)}
+                        onHoverIn={() => setTempRating(i + 1)}
+                        onHoverOut={() => setTempRating(0)}
+                    />
                 ))}
             </div>
-            <p>Total Raring : {starRating}</p>
+            <p>Total Raring : {tempRating || starRating || ""}</p>
         </div>
     )
 }
@@ -33,17 +44,38 @@ const startStyle = {
     cursor: "pointer",
 }
 
-const Start = ({ onRate }) => {
+const Start = ({ onRate, full, onHoverIn, onHoverOut }) => {
     return (
-        <span style={startStyle} onClick={onRate}>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#000"
-                stroke="#000"
-            >
-                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-            </svg>
+        <span
+            style={startStyle}
+            onClick={onRate}
+            onMouseEnter={onHoverIn}
+            onMouseLeave={onHoverOut}
+        >
+            {full ? (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="#000"
+                    stroke="#000"
+                >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+            ) : (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="#000"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="{2}"
+                        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    />
+                </svg>
+            )}
         </span>
     )
 }
