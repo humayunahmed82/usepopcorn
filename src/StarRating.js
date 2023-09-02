37df9fb3fd89178987c4ce7dad55dde3201cb364
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2023-09-02 18:15:51 +0600

    Use Popcron Rating Done

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,21 +1,45 @@
-import { useState } from "react"
+import { useState } from "react";
+import PropTypes from "prop-types";
 
-const startRow = {
+const startRowStyle = {
     display: "flex",
     flexDirection: "row",
-}
+};
 
-const StarRating = ({ maxRating = 5 }) => {
-    const [starRating, setStartRating] = useState(0)
-    const [tempRating, setTempRating] = useState(0)
+const startRatingStyle = {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: "10px",
+};
+
+const StarRating = ({
+    maxRating = 5,
+    color = "#fcc419",
+    size = 48,
+    className = "",
+    messages = [],
+    defaultRating = 0,
+    onSetRating,
+}) => {
+    const [starRating, setStartRating] = useState(defaultRating);
+    const [tempRating, setTempRating] = useState(0);
 
     const handelRated = (rating) => {
-        setStartRating(rating)
-    }
+        setStartRating(rating);
+        onSetRating(rating);
+    };
+
+    const textStyle = {
+        lineHeight: "1",
+        margin: 0,
+        color,
+        fontSize: `${size / 1.8}px`,
+    };
 
     return (
-        <div>
-            <div style={startRow}>
+        <div style={startRatingStyle} className={className}>
+            <div style={startRowStyle}>
                 {Array.from({ length: maxRating }, (_, i) => (
                     <Start
                         key={i}
@@ -27,24 +51,40 @@ const StarRating = ({ maxRating = 5 }) => {
                         onRate={() => handelRated(i + 1)}
                         onHoverIn={() => setTempRating(i + 1)}
                         onHoverOut={() => setTempRating(0)}
+                        color={color}
+                        size={size}
                     />
                 ))}
             </div>
-            <p>Total Raring : {tempRating || starRating || ""}</p>
+            <p style={textStyle}>
+                {messages.length === maxRating
+                    ? messages[tempRating ? tempRating - 1 : starRating - 1]
+                    : tempRating || starRating || ""}
+            </p>
         </div>
-    )
-}
+    );
+};
 
-export default StarRating
+StarRating.propTypes = {
+    maxRating: PropTypes.number,
+    defaultRating: PropTypes.number,
+    color: PropTypes.string,
+    size: PropTypes.number,
+    className: PropTypes.string,
+    messages: PropTypes.array,
+    onSetRating: PropTypes.func,
+};
 
-const startStyle = {
-    width: "40px",
-    height: "40px",
-    display: "block",
-    cursor: "pointer",
-}
+export default StarRating;
 
-const Start = ({ onRate, full, onHoverIn, onHoverOut }) => {
+const Start = ({ onRate, full, onHoverIn, onHoverOut, color, size }) => {
+    const startStyle = {
+        width: `${size}px`,
+        height: `${size}px`,
+        display: "block",
+        cursor: "pointer",
+    };
+
     return (
         <span
             style={startStyle}
@@ -56,8 +96,8 @@ const Start = ({ onRate, full, onHoverIn, onHoverOut }) => {
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
-                    fill="#000"
-                    stroke="#000"
+                    fill={color}
+                    stroke={color}
                 >
                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
@@ -66,7 +106,7 @@ const Start = ({ onRate, full, onHoverIn, onHoverOut }) => {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke="#000"
+                    stroke={color}
                 >
                     <path
                         strokeLinecap="round"
@@ -77,5 +117,5 @@ const Start = ({ onRate, full, onHoverIn, onHoverOut }) => {
                 </svg>
             )}
         </span>
-    )
-}
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-import StarRating from "./StarRating"
+import StarRating from "./StarRating";
 // import "./index.css"
 // import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const TestRating = () => {
+    const [movieRating, setMovieRating] = useState(0);
+
+    return (
+        <div className="">
+            <StarRating
+                maxRating={10}
+                color="blue"
+                onSetRating={setMovieRating}
+            />
+            ;<p>This movie was rated {movieRating} Start</p>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         {/* <App /> */}
 
-        <StarRating maxRating={10} />
+        <StarRating messages={["Terrible", "bad", "Okay", "Good", "Amazing"]} />
+        <StarRating maxRating={5} className="Text" defaultRating={3} />
+        <TestRating />
     </React.StrictMode>
-)
+);
